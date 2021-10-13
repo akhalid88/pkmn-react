69b7aec9d3bd51ae30754d875e1eb4db4532b1d0
Muhammad A Khalid <akhalid.code@gmail.com>
2021-10-13 09:56:44 -0700

    dd get pokemon functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { getAllPokemon, getPokemon } from './services/pokemon';
+import Card from './components/Card';
+import Navbar from './components/Navbar';
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+  const [nextUrl, setNextUrl] = useState('');
+  const [prevUrl, setPrevUrl] = useState('');
+  const [loading, setLoading] = useState(true);
+  const initialUrl = 'https://pokeapi.co/api/v2/pokemon';
+
+  useEffect(() => {
+    async function fetchData() {
+      let response = await getAllPokemon(initialUrl);
+      setNextUrl(response.next);
+      setPrevUrl(response.previous);
+      await loadingPokemon(response.results);
+      setLoading(false);
+    }
+    fetchData();
+  }, []);
+
+  const loadingPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map(async pokemon => {
+        let pokemonRecord = await getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+
+    setPokemonData(_pokemonData);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? <h1>Loading...</h1> : (
+        <>
+          <div className="grid-container">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon} />
+            })}
+          </div>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/Card/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+function Card() {
+	return (
+		<div className="card">
+			<div className="card-img">
+				{/* <img src="" alt="" /> */}
+			</div>
+			<div  className="card-name">
+				{/* pokemon name placeholder */}
+			</div>
+		</div>
+	)
+}
+
+export default Card;
